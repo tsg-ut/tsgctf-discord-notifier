1bcfef7dc11f5a02e1e574cbab4028a33cd48bfa
Koki Takahashi <hakatasiloving@gmail.com>
2021-10-03 00:39:26 +0900

    Add team_password_reset

---
+++ team_password_reset.ts
@@ -0,0 +1,114 @@
+import dotenv from 'dotenv';
+import axios from 'axios';
+import readline from 'readline';
+import get from 'lodash/get';
+import Mailgun from 'mailgun-js';
+import {stripIndent} from 'common-tags';
+import {nanoid} from 'nanoid';
+
+dotenv.config();
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+const {CTFD_HOST, CTFD_SESSION, MAILGUN_API_KEY} = process.env;
+
+const mailgun = Mailgun({apiKey: MAILGUN_API_KEY!, domain: 'hakatashi.com'});
+
+(async () => {
+	const teamId = await new Promise((resolve) => {
+		rl.question('Enter team id: ', resolve);
+	});
+
+	console.log('');
+	console.log(`Team id: ${teamId}`);
+	console.log('');
+
+	await new Promise<void>((resolve) => {
+		rl.question('Is this ok? [yN] ', (answer) => {
+			if (answer.toLowerCase() === 'y') {
+				rl.close();
+				resolve();
+			} else {
+				process.exit();
+			}
+		});
+	});
+
+	console.log('Getting Team configuration...');
+
+	const emails: string[] = ['pr-log@mail-hook.tsg.ne.jp'];
+
+	console.log('Getting CSRF token...');
+	const {data} = await axios.get(`${CTFD_HOST}/admin/notifications`, {
+		headers: {
+			Cookie: `session=${CTFD_SESSION}`,
+		},
+	});
+	const [, token] = data.match(/'csrfNonce'\s*:\s*"(.+?)"/);
+	console.log(`Got CSRF token: ${token}`);
+
+	console.log('Posting notification...');
+	const {data: result} = await axios.get(`${CTFD_HOST}/api/v1/teams/${teamId}/members`, {
+		headers: {
+			Cookie: `session=${CTFD_SESSION}`,
+			'CSRF-Token': token,
+		},
+	});
+
+	const members = get(result, 'data', []);
+	console.log('CTFd team members:', members);
+
+	for (const member of members) {
+		const {data: result} = await axios.get(`${CTFD_HOST}/api/v1/users/${member}`, {
+			headers: {
+				Cookie: `session=${CTFD_SESSION}`,
+				'CSRF-Token': token,
+			},
+		});
+		const email = get(result, ['data', 'email']);
+		emails.push(email);
+	}
+
+	console.log(emails);
+
+	const teamPassword = nanoid(16);
+	console.log({teamPassword});
+
+	const {data: patchResult} = await axios.patch(`${CTFD_HOST}/api/v1/teams/${teamId}`, JSON.stringify({
+		password: teamPassword,
+	}), {
+		headers: {
+			Cookie: `session=${CTFD_SESSION}`,
+			'CSRF-Token': token,
+			'Content-Type': 'application/json',
+		},
+	});
+
+	const teamName = get(patchResult, ['data', 'name']);
+
+	const content = stripIndent`
+		Hi team ${teamName} <br>
+
+		We reset your team password. The new password is ${teamPassword} <br>
+
+		TSG
+	`;
+
+	const mailResult = await new Promise((resolve) => {
+		mailgun.messages().send({
+			from: 'TSG <admin@tsg.ne.jp>',
+			to: 'info@tsg.ne.jp',
+			bcc: emails,
+			subject: 'TSG CTF 2021 team password reset',
+			text: content,
+			html: content,
+		}, (error, body) => {
+			resolve(body);
+		});
+	});
+
+	console.log(mailResult);
+})();
